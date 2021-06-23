c5e8c42e249e471c95c63fc13a673cd7bf277e6c
ihsbramn <abdurahmanihsan25@gmail.com>
2021-06-23 10:48:34 +0700

    show id after insert | view by id

--- siparmabot.py
+++ siparmabot.py
@@ -18,7 +18,8 @@ chatid = ('-478142407')
 # auth
 
 
-bot.sendMessage(chatid, 'Selamat Datang! ')
+bot.sendMessage(
+    chatid, 'Selamat datang di SIParmaBot \nSiParma Siap Membantu anda! 😉 ')
 
 
 def handle(msg):
@@ -26,18 +27,35 @@ def handle(msg):
     args = text.split()
     command = args[0]
     response = bot.getUpdates()
+    status = ('open')
 
     if command == '/start':
         bot.sendMessage(
             chatid, 'Selamat datang di SIParmaBot \nSiParma Siap Membantu anda! 😉 ')
 
     if command == '/moban':
-        host = str(args[1]), str(args[2]), str(args[3])
+        host = str(args[1]), str(args[2]), str(status)
         cursor.execute(
-            "INSERT INTO moban(title,status,date) VALUE('%s','%s','%s')" % (host))
+            "INSERT INTO reports(report_title,report_value,report_status) VALUE('%s','%s','%s')" % (host))
         db.commit()
         bot.sendMessage(
-            chatid, 'Data Diterima!')
+            chatid, 'Moban Diterima! 👍\nDengan ID : ' + str(cursor.lastrowid) + '\nStatus : ' + status)
+
+    if command == '/cek':
+        host = str(args[1])
+        cursor.execute(
+            "SELECT * FROM reports WHERE id = '%s'" % (host))
+        hasil = cursor.fetchall()
+
+        if cursor.rowcount > 0:
+            for row in hasil:
+                output = "Data Ditemukan " + "\nTitle : " + \
+                    row[1] + "\nIsi : " + row[2] + "\nStatus : " + row[3]
+        else:
+            output = "Title : " + host + " Tidak ditemukan"
+
+        bot.sendMessage(
+            chatid, output)
 
     if command == '/test':
         bot.sendMessage(
