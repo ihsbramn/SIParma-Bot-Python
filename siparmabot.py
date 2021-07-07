6cc67ec0b940af2e6f45c1b4865453c08480fb49
ihsbramn <abdurahmanihsan25@gmail.com>
2021-07-07 15:18:00 +0700

    command cek additional

--- siparmabot.py
+++ siparmabot.py
@@ -1,6 +1,7 @@
 import telepot
 import time
 import pymysql
+import json
 from telepot.loop import MessageLoop
 from pprint import pprint
 
@@ -13,8 +14,8 @@ db = pymysql.connect(
 cursor = db.cursor()
 
 # auth
-bot = telepot.Bot('BOT_TOKEN')
-chatid = ('CHAT_ID')
+bot = telepot.Bot('1786482522:AAEKQOpHgMgtWV_IVpGv9Ldz6c_j57Eal04')
+chatid = ('-478142407')
 # auth
 
 
@@ -25,6 +26,8 @@ bot.sendMessage(
 def handle(msg):
     sender_id = msg['from']['id']
     sender_username = msg['from']['username']
+    sender_name = msg['from']['first_name'] + ' ' + msg['from']['last_name']
+    id_group = msg['chat']['id']
     text = msg['text']
     args = text.split()
     command = args[0]
@@ -36,9 +39,9 @@ def handle(msg):
 
     if command == '/moban':
         host = str(args[1]), str(args[2]), str(
-            args[3]), str(args[4]), str(sender_id), str(sender_username), str(status)
+            args[3]), str(args[4]), str(sender_id), str(sender_username), str(sender_name), str(status)
         cursor.execute(
-            "INSERT INTO reports(report_type,report_number,report_value,report_detail,report_idsender,report_sender,report_status) VALUE('%s','%s','%s','%s','%s','%s','%s')" % (host))
+            "INSERT INTO reports(report_type,report_number,report_value,report_detail,report_idsender,report_sender,sender_name,report_status) VALUE('%s','%s','%s','%s','%s','%s','%s','%s')" % (host))
         db.commit()
         bot.sendMessage(
             chatid, 'Moban Diterima! 👍' + '\n' + '\nID Moban : ' + str(cursor.lastrowid) + '\nID Pengirim : ' + str(sender_id) + '\nUsername Pengirim : ' + '@'+sender_username + '\n \nStatus : ' + status)
@@ -52,9 +55,12 @@ def handle(msg):
         if cursor.rowcount > 0:
             for row in hasil:
                 output = "Data Ditemukan 😉" + '\n' + "\nID Moban : " + \
-                    str(row[0]) + "\nJenis Order : " + row[1] + "\nNo Order : " + row[2] + \
+                    str(row[0]) + "\nJenis Order : " + row[1] + "\nSC ID : " + row[2] + \
                     '\nUsername Pelapor : ' + '@' + \
-                    row[6] + '\n \nStatus : ' + row[7]
+                    row[6] + '\nNama Pelapor : ' + \
+                    row[7] + '\nKeluhan : '+row[3] + ' ' + \
+                    row[4] + '\n\n(OPEN->OGP) : ' + str(row[9]) + '\n(OGP->ESKALASI) : ' + str(row[10]) + '\n(OGP->CLOSED) : ' + str(row[11]) + '\n(ESKALASI->CLOSED) : ' + str(row[12]) + '\n\nWaktu Terakhir Update : \n' + \
+                    str(row[18]) + '\nStatus : ' + row[8]
         else:
             output = "Data ID Moban " + host + " Tidak ditemukan ☹️"
 
@@ -62,8 +68,9 @@ def handle(msg):
             chatid, output)
 
     if command == '/testid':
+        print(json.dumps(msg, indent=4, sort_keys=True))
         bot.sendMessage(
-            chatid, 'ID Pengirim : ' + str(sender_id) + '\nUsername Pengirim : ' + '@'+sender_username)
+            chatid, 'ID Group : ' + str(id_group) + '\nID Pengirim : ' + str(sender_id) + '\nUsername Pengirim : ' + '@'+sender_username + '\nNama Pengirim : ' + str(sender_name))
 
     if command == '/help':
         bot.sendMessage(
